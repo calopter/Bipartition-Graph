1e5e4de381203b655a7a15a79d3f4f4b9fea8479
Mira <calopter@gmail.com>
2020-04-07 18:57:20 -0700

    possible_bipartition

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,28 @@
+def bipartite? adj, i, colors
+  q = [i]
+  colors[i] = 1
 
-def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  while !q.empty?
+    u = q.shift
+    adj[u].each do |v|
+      if colors[v] == -1
+        colors[v] = 1 - colors[u]
+        q << v
+      elsif colors[u] == colors[v]
+        return false
+      end
+    end
+  end
+  return true
+end
+
+def possible_bipartition dislikes
+  colors = Array.new(dislikes.length, -1)
+
+  dislikes.each_index do |i|
+    if colors[i] == -1
+      return false unless bipartite? dislikes, i, colors
+    end
+  end
+  return true
 end
